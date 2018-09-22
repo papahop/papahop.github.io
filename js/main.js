fff75a8b1798e19d4fefe0115a72bb01b2377862
Anders Bälter <anders@84codes.com>
2018-09-22 16:38:25 +0200

    More content

--- js/main.js
+++ js/main.js
@@ -16,7 +16,21 @@ const smoothScroll = function () {
   })
 }
 
+const facebookPagePlugin = function () {
+  (function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0]
+    if (d.getElementById(id)) {
+      return
+    }
+    js = d.createElement(s)
+    js.id = id
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1'
+    fjs.parentNode.insertBefore(js, fjs)
+  }(document, 'script', 'facebook-jssdk'))
+}
+
 window.addEventListener('load', function () {
   smoothScroll()
+  facebookPagePlugin()
   // window.onscroll = scrollHandler
 }, false)
